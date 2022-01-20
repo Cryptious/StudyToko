b08580de224d07a163f36f05df03f3644d90fdca
Cryptious <cryptiousw@gmail.com>
2022-01-20 10:36:22 +0700

    add API Simpan data Barang

--- app/Http/Controllers/Utama.php
+++ app/Http/Controllers/Utama.php
@@ -6,9 +6,30 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\M_Barang;
+
 class Utama extends Controller
 {
     public function index(){
         return view('utama');
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'file' => 'required|max:2048'
+        ]);
+        $file=$request->file('file');
+        $nama_file = time()."_".$file->getClientOriginalName();
+        $tujuan_upload = 'data_file';
+        if ($file->move($tujuan_upload, $nama_file)) {
+            $data = M_Barang::create([
+                'nama_produk' => $request->nama_produk,
+                'harga' => $request->harga,
+                'gambar' => $nama_file
+            ]);
+            $res['message'] = "Success!";
+            $res['values'] = $data;
+            return response($res);
+        }
     }
 }
