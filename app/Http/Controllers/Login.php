98e6fa68314da32525b6388f54cdde8517644fec
Cryptious <cryptiousw@gmail.com>
2022-01-20 13:41:59 +0700

    add login

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -21,4 +21,18 @@ class Login extends Controller
         ]);
         return redirect('/Login');
     }
+
+    public function Masuk(Request $request){
+        $user = DB::table('tbl_user')->where('email',$request->email)->first();
+        if($user->password == $request->password){
+            $request->session()->put('id',$user->id);
+            echo 'Data Disimpan dengan session id ='. $request->session()->get('id');
+            return redirect('/');
+            
+        }else{
+            echo "anda Gagal Login";
+            return redirect('/Login');
+
+        }
+    }
 }
